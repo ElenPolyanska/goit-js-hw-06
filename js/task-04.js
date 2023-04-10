1ccf8cad12da3584bc55c5112c5b660a591470e8
ElenPolyanska <118685558+ElenPolyanska@users.noreply.github.com>
2023-04-10 21:02:45 +0300

    #4e

--- js/task-04.js
+++ js/task-04.js
@@ -1,26 +1,42 @@
-const counter = {
-    value: 0,
-    increment() {
-        this.value += 1;
-    },
+// const counter = {
+//     value: 0,
+//     increment() {
+//         this.value += 1;
+//     },
 
-    decrement() {
-        this.value -= 1;
-    },
-};
+//     decrement() {
+//         this.value -= 1;
+//     },
+// };
 
-const decrementBtn =
-    document.querySelector('[data-action="decrement"]');
-const incrementBtn =
-    document.querySelector('[data-action="increment"]');
-const counterValue =
-    document.querySelector('#value');
+// const decrementBtn =
+//     document.querySelector('[data-action="decrement"]');
+// const incrementBtn =
+//     document.querySelector('[data-action="increment"]');
+// const counterValue =
+//     document.querySelector('#value');
 
-decrementBtn.addEventListener('click', function () { 
-    counter.decrement();
-    counterValue.textContent = counter.value;
+// decrementBtn.addEventListener('click', function () { 
+//     counter.decrement();
+//     counterValue.textContent = counter.value;
+// });
+// incrementBtn.addEventListener('click', function () { 
+//     counter.increment();
+//     counterValue.textContent = counter.value;
+// })
+
+
+let counterValue=15;
+
+const spanEl=document.querySelector('#value');
+const btnDec=document.querySelector('[data-action="decrement"]');
+const btnInc=document.querySelector('[data-action="increment"]');
+
+btnDec.addEventListener('click', ()=>{
+    counterValue--
+    spanEl.textContent=counterValue;
 });
-incrementBtn.addEventListener('click', function () { 
-    counter.increment();
-    counterValue.textContent = counter.value;
-})
+btnInc.addEventListener('click', ()=>{
+    counterValue++
+    spanEl.textContent=counterValue;
+});
